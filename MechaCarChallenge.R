468265c6a7bdf4890f65ed6788adf409544d3416
lauren1478 <lauren.rishel@utexas.edu>
2022-10-11 22:30:15 -0500

    commit deliverables

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,3 +1,4 @@
+# Deliverable 1
 library(dplyr)
 ?read.csv()
 mecha_table <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
@@ -7,3 +8,28 @@ mmpg_model <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_
 
 #generate summary statistics
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = mecha_table)) #generate summary statistics
+
+# Deliverable 2
+?read.csv()
+suspension_table <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+# total summary
+summary_suspension <- suspension_table %>% summarize(Mean_PSI=mean(PSI),Median_PSI=median(PSI),Var_PSI=var(PSI),SD_PSI=sd(PSI), .groups = 'keep') 
+
+#use groupby
+lot_summary <- suspension_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI),Median_PSI=median(PSI),Var_PSI=var(PSI),SD_PSI=sd(PSI), .groups = 'keep') 
+
+# Deliverable 3
+?t.test()
+t.test(suspension_table$PSI, mu=1500)
+
+# example 
+#subset(airquality, Day == 1, select = -Temp)
+t.test(subset(suspension_table, Manufacturing_Lot == 'Lot1')$PSI, mu=1500)
+
+t.test(subset(suspension_table, Manufacturing_Lot == 'Lot2')$PSI, mu=1500)
+
+t.test(subset(suspension_table, Manufacturing_Lot == 'Lot3')$PSI, mu=1500)
+
+
+#Deliverable 4
